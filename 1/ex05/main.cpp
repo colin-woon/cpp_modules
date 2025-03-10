0aa4b8ce496c3326f6467adc46ca2f8896a15dae
Colin Woon <colin.wyj@gmail.com>
2025-03-10 17:58:44 +0800

    cpp01-ex05: done

---
+++ 1/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 17:55:23 by cwoon             #+#    #+#             */
+/*   Updated: 2025/03/10 17:56:21 by cwoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.class.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return (0);
+}
