8582e9d91fd84f5153f97a78b0fec623b3458b4e
Colin Woon <colin.wyj@gmail.com>
2025-03-14 21:14:36 +0800

    cpp02-ex03: fixed overflow error, but with limitations

--- 2/ex03/bsp.cpp
+++ 2/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 17:12:48 by cwoon             #+#    #+#             */
-/*   Updated: 2025/03/14 19:16:07 by cwoon            ###   ########.fr       */
+/*   Updated: 2025/03/14 21:13:48 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ static Fixed getTriangleArea(Point const i, Point const j, Point const point)
 				  (point.getX() * (i.getY() - j.getY()))) /
 				 Fixed(2);
 
-	std::cout << "area" << area << std::endl;
 	if (area < Fixed(0))
 		area = area * Fixed(-1);
 
+	// std::cout << "Area: " << area << std::endl;
 	return area;
 }
 
@@ -37,35 +37,30 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	Fixed area_apc = getTriangleArea(a, point, c);
 	Fixed area_abp = getTriangleArea(a, b, point);
 
-	// std::cout << "Area ABC: " << area_abc << std::endl;
-	// std::cout << "Area summed: " << area_pbc + area_apc + area_abp << std::endl;
-
-	// if (area_abc == Fixed(0) ||
-	// 	area_pbc == Fixed(0) ||
-	// 	area_apc == Fixed(0) ||
-	// 	area_abp == Fixed(0))
-	// 	return false;
-
-	// if (area_abc == (area_pbc + area_apc + area_abp))
-	// 	return true;
-	// else
-	// 	return false;
-
 	// Define epsilon as smallest possible value in our fixed-point format
 	Fixed epsilon(0.00390625f); // 1/256 (with 8 fractional bits)
 
 	// Check for zero areas (point on edge/vertex) with epsilon
-	if (area_abc < epsilon ||
-		area_pbc < epsilon ||
-		area_apc < epsilon ||
-		area_abp < epsilon)
+	if ((area_abc < epsilon) ||
+		(area_pbc < epsilon) ||
+		(area_apc < epsilon) ||
+		(area_abp < epsilon))
+	{
+		std::cout << "Point is on the edge or vertex of the triangle" << std::endl;
 		return false;
+	}
 
 	// Compare areas with epsilon tolerance
 	Fixed diff = (area_pbc + area_apc + area_abp) - area_abc;
-	if (diff < Fixed(-1))
+
+	// Shows the inaccuracy of floating-point numbers
+	// std::cout << "Area ABC: " << area_abc << std::endl;
+	// std::cout << "Area ALL: " << area_pbc + area_apc + area_abp << std::endl;
+	// std::cout << "Diff: " << diff << std::endl;
+
+	if (diff < Fixed(0))
 		diff = diff * Fixed(-1);
-	if (diff < epsilon && diff > (epsilon * Fixed(-1)))
+	if (diff <= epsilon)
 		return true;
 	return false;
 }
--- 2/ex03/main.cpp
+++ 2/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 16:49:45 by cwoon             #+#    #+#             */
-/*   Updated: 2025/03/14 18:51:59 by cwoon            ###   ########.fr       */
+/*   Updated: 2025/03/14 20:57:59 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,38 @@ int main(void)
 	Point a(0, 0);
 	Point b(4.2f, 0);
 	Point c(0, 4.2f);
-	Point point_on_hypotenus(2.1f, 2.1f);
+	Point point_on_hypotenuse(2.1f, 2.1f);
+	Point point_on_vertex(0, 0);
 	Point inside(1, 1);
 	Point outside(5, 5);
 
-	// if (bsp(a, b, c, point_on_hypotenus))
-	// 	std::cout << "Point is inside the triangle" << std::endl;
-	// else
-	// 	std::cout << "Point is outside the triangle" << std::endl;
+	// Point a(0, 0);
+	// Point b(4, 0);					 // Changed from 4.2 to 4
+	// Point c(0, 4);					 // Changed from 4.2 to 4
+	// Point point_on_hypotenuse(2, 2); // Exact point on hypotenuse, binary-friendly
+	// Point point_on_vertex(0, 0);
+	// Point inside(1, 1);
+	// Point outside(5, 5);
+
+	if (bsp(a, b, c, point_on_hypotenuse))
+		std::cout << "Point is inside the triangle" << std::endl;
+	else
+		std::cout << "Point is outside the triangle" << std::endl;
 
 	if (bsp(a, b, c, inside))
 		std::cout << "Point is inside the triangle" << std::endl;
 	else
 		std::cout << "Point is outside the triangle" << std::endl;
 
-	// if (bsp(a, b, c, outside))
-	// 	std::cout << "Point is inside the triangle" << std::endl;
-	// else
-	// 	std::cout << "Point is outside the triangle" << std::endl;
+	if (bsp(a, b, c, outside))
+		std::cout << "Point is inside the triangle" << std::endl;
+	else
+		std::cout << "Point is outside the triangle" << std::endl;
+
+	if (bsp(a, b, c, point_on_vertex))
+		std::cout << "Point is inside the triangle" << std::endl;
+	else
+		std::cout << "Point is outside the triangle" << std::endl;
+
 	return 0;
 }
