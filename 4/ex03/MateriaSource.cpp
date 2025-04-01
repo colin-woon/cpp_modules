e27378ad52e3c52c039ff0fd2e78c0aff46b76ea
Colin Woon <colin.wyj@gmail.com>
2025-04-01 20:13:42 +0800

    feat: added MateriaSource concrete class implementaiton

--- 4/ex03/MateriaSource.cpp
+++ 4/ex03/MateriaSource.cpp
@@ -1,17 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 19:58:19 by cwoon             #+#    #+#             */
+/*   Updated: 2025/04/01 20:13:18 by cwoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "MateriaSource.hpp"
 
-MateriaSource::MateriaSource() {}
+MateriaSource::MateriaSource() : _storage(), _storageCount(0) { std::cout << "MateriaSource default constructor called" << std::endl; }
 
-MateriaSource::MateriaSource(const MateriaSource& other) {
-    this->value = other.value;
+MateriaSource::MateriaSource(const MateriaSource &other) : _storageCount(other.getStorageCount())
+{
+	for (int i = 0; i < _storageCount; i++)
+	{
+		if (other.getMateria(i))
+			this->_storage[i] = other.getMateria(i)->clone();
+		else
+			this->_storage[i] = NULL;
+	}
 }
 
-MateriaSource& MateriaSource::operator=(const MateriaSource& other) {
-    if (this != &other)
-    {
-	this->value = other.value;
-    }
-    return *this;
+MateriaSource &MateriaSource::operator=(const MateriaSource &other)
+{
+	for (int i = 0; i < _storageCount; i++)
+	{
+		if (this->_storage[i])
+			delete this->_storage[i];
+		if (other.getMateria(i))
+			this->_storage[i] = other.getMateria(i)->clone();
+		else
+			this->_storage[i] = NULL;
+	}
+	this->_storageCount = other.getStorageCount();
+	return *this;
 }
 
-MateriaSource::~MateriaSource() {}
+MateriaSource::~MateriaSource() { std::cout << "MateriaSource destructor called" << std::endl; }
+
+AMateria *MateriaSource::getMateria(int index) const
+{
+	if (index < 0 || index >= _storageCount)
+		return NULL;
+	return this->_storage[index];
+}
+
+int MateriaSource::getStorageCount() const
+{
+	return this->_storageCount;
+}
+
+void MateriaSource::incrementStorageCount()
+{
+	if (this->_storageCount < 4)
+		this->_storageCount++;
+	else
+		std::cout << "MateriaSource storage is full" << std::endl;
+}
+
+void MateriaSource::decrementStorageCount()
+{
+	if (this->_storageCount > 0)
+		this->_storageCount--;
+	else
+		std::cout << "MateriaSource storage is empty" << std::endl;
+}
+
+void MateriaSource::learnMateria(AMateria *m)
+{
+	if (!m)
+		return;
+	if (this->_storageCount < 4)
+	{
+		this->_storage[this->_storageCount] = m->clone();
+		this->incrementStorageCount();
+	}
+	else
+		std::cout << "MateriaSource storage is full" << std::endl;
+}
+
+AMateria *MateriaSource::createMateria(std::string const &type)
+{
+	for (int i = 0; i < this->_storageCount; i++)
+	{
+		if (this->_storage[i]->getType() == type)
+			return this->_storage[i]->clone();
+	}
+	return NULL;
+}
--- 4/ex03/MateriaSource.hpp
+++ 4/ex03/MateriaSource.hpp
@@ -1,13 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MateriaSource.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 19:57:23 by cwoon             #+#    #+#             */
+/*   Updated: 2025/04/01 20:09:35 by cwoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MATERIASOURCE_HPP
 #define MATERIASOURCE_HPP
 
+#include "IMateriaSource.hpp"
+
 class MateriaSource
 {
+private:
+	AMateria *_storage[4];
+	int _storageCount;
+
 public:
-    MateriaSource();                           
-    MateriaSource(const MateriaSource& other);
-    MateriaSource& operator=(const MateriaSource& other);
-    ~MateriaSource();                         
+	MateriaSource();
+	MateriaSource(const MateriaSource &other);
+	MateriaSource &operator=(const MateriaSource &other);
+	virtual ~MateriaSource();
+
+	AMateria *getMateria(int index) const;
+	int getStorageCount() const;
+	void incrementStorageCount();
+	void decrementStorageCount();
+
+	void learnMateria(AMateria *m);
+	AMateria *createMateria(std::string const &type);
 };
 
 #endif
