872dcea3c997c874b9635ac612d433efae95d8da
Colin Woon <colin.wyj@gmail.com>
2025-08-28 18:13:52 +0800

    cpp7-ex1: added eval testcase

--- 7/ex01/main.cpp
+++ 7/ex01/main.cpp
@@ -42,3 +42,39 @@ int main(void)
 
 	return 0;
 }
+
+// class Awesome
+// {
+// public:
+// 	Awesome(void) : _n(42) { return; }
+// 	int get(void) const { return this->_n; }
+
+// private:
+// 	int _n;
+// };
+
+// std::ostream &operator<<(std::ostream &o, Awesome const &rhs)
+// {
+// 	o << rhs.get();
+// 	return o;
+// }
+
+// template <typename T>
+// void print(T &x)
+// {
+// 	std::cout << x << std::endl;
+// 	return;
+// }
+
+// int main()
+// {
+// 	// int tab[] = {0, 1, 2, 3, 4};
+// 	std::string strtab[] = {"0", "1", "2", "3", "4"};
+// 	char chartab[] = {'0', '1', '2', '3', '4'};
+// 	Awesome tab2[5];
+
+// 	iter(chartab, 5, print<const int>);
+// 	iter(tab2, 5, print<Awesome>);
+
+// 	return 0;
+// }
